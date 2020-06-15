40e980f369b7cda3cda103fce56c99c88ebd45ff
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2020-06-15 13:58:47 -0500

    Added standard input

--- Pregunta1.cpp
+++ Pregunta1.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 std::vector<int> getBlocks(std::vector<int> array);
+std::vector<int> getVectorFromString(std::string);
 double GreedyMinMatching(std::vector<int> A, std::vector<int> B, std::vector<std::pair<int,int>>* matching);
 
 int main() {
-    std::vector<int> A = {0,1,1,1,0,0,1,0,1,1,0,1,1,0,1,1,1,0,1,0};
-    std::vector<int> B = {0,0,1,1,0,1,1,0,0,0,1,1,1,1,1,0,0,1,1,0};
+    std::string stringA, stringB;
+    std::getline(std::cin, stringA);
+    std::getline(std::cin, stringB);
+    std::vector<int> A = getVectorFromString(stringA);
+    std::vector<int> B = getVectorFromString(stringB);
+
     std::vector<std::pair<int,int>> matching;
     double peso  = GreedyMinMatching(A, B, &matching);
     for (auto pair : matching) {
@@ -80,4 +86,13 @@ double GreedyMinMatching(std::vector<int> A, std::vector<int> B, std::vector<std
         peso += (double)blocksA[i-1] / den;
     }
     return peso;
+}
+
+std::vector<int> getVectorFromString(std::string s) {
+    std::vector<int> v;
+    int i;
+    std::stringstream stream(s);
+    while (stream >> i)
+        v.push_back(i);
+    return v;
 }
