4246f302b41a83997f8b7ea1a2d8c2e621c677fd
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2020-06-19 14:57:11 -0500

    Update Pregunta5.cpp

--- Pregunta5.cpp
+++ Pregunta5.cpp
@@ -6,7 +6,7 @@ std::vector<int> getBlocks(std::vector<int> array);
 std::vector<int> getVectorFromString(std::string);
 double DPMinMatching(std::vector<int> A, std::vector<int> B, std::vector<std::pair<int,int>>* matching);
 void DPMinMatchingRec(std::vector<int> blocksA, std::vector<int> blocksB, int i, int j,
-                      std::vector<std::pair<int,int>>** matching, double** memo);
+                      std::vector<std::pair<int,int>>** matching, double** peso);
 
 int main() {
     std::string stringA, stringB;
@@ -46,68 +46,64 @@ double DPMinMatching(std::vector<int> A, std::vector<int> B, std::vector<std::pa
     std::vector<int> blocksB = getBlocks(std::move(B));
     int m = blocksA.size();
     int n = blocksB.size();
-    double** memo;
-    std::vector<std::pair<int,int>>** matching;
-    memo = new double*[m];
-    matching = new std::vector<std::pair<int,int>>*[m];
+    auto peso = new double*[m];
+    auto matching = new std::vector<std::pair<int,int>>*[m];
     for (int i = 0; i < m; ++i) {
-        memo[i] = new double[n];
+        peso[i] = new double[n];
         matching[i] = new std::vector<std::pair<int, int>>[n];
-    }
-    for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j)
-            DPMinMatchingRec(blocksA, blocksB, i, j, matching, memo);
+            DPMinMatchingRec(blocksA, blocksB, i, j, matching, peso);
     }
     *Matching = matching[m-1][n-1];
-    return memo[m-1][n-1];
+    return peso[m-1][n-1];
 }
 
 void DPMinMatchingRec(std::vector<int> blocksA, std::vector<int> blocksB, int i, int j,
-                      std::vector<std::pair<int,int>>** matching, double** memo) {
+                      std::vector<std::pair<int,int>>** matching, double** peso) {
     if (i == 0) {
-        double peso = blocksA[0];
+        double w = blocksA[0];
         int den = 0;
         for (int jj = 0; jj <= j; ++jj) {
             den += blocksB[jj];
             matching[0][j].emplace_back(1, jj + 1);
         }
-        peso /= den;
-        memo[0][j] = peso;
+        w /= den;
+        peso[0][j] = w;
     } else if (j == 0) {
-        double peso = 0;
+        double w = 0;
         for (int ii = 0; ii <= i; ++ii) {
-            peso += blocksA[ii];
+            w += blocksA[ii];
             matching[i][0].emplace_back(ii + 1, 1);
         }
-        peso /= blocksB[0];
-        memo[i][0] = peso;
+        w /= blocksB[0];
+        peso[i][0] = w;
     } else {
-        memo[i][j] = INF;
+        peso[i][j] = INF;
         for (int x = 0; x < i; ++x) { //Agrupar
-            double agrupar = memo[x][j-1];
+            double w = peso[x][j-1];
             std::vector<std::pair<int, int>> newMatching(matching[x][j-1]);
             double num = 0;
             for (int n = x + 1; n <= i; ++n) {
                 num += blocksA[n];
                 newMatching.emplace_back(n + 1, j + 1);
             }
-            agrupar += num / blocksB[j];
-            if (agrupar < memo[i][j]) {
-                memo[i][j] = agrupar;
+            w += num / blocksB[j];
+            if (w < peso[i][j]) {
+                peso[i][j] = w;
                 matching[i][j] = newMatching;
             }
         }
         for (int y = 0; y < j; ++y) { //Dividir
-            double dividir = memo[i-1][y];
+            double w = peso[i-1][y];
             std::vector<std::pair<int, int>> newMatching(matching[i-1][y]);
             double den = 0;
             for (int n = y + 1; n <= j; ++n) {
                 den += blocksB[n];
                 newMatching.emplace_back(i + 1, n + 1);
             }
-            dividir += blocksA[i] / den;
-            if (dividir < memo[i][j]) {
-                memo[i][j] = dividir;
+            w += blocksA[i] / den;
+            if (w < peso[i][j]) {
+                peso[i][j] = w;
                 matching[i][j] = newMatching;
             }
         }
